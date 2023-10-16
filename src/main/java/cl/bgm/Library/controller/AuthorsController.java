213ed917db79bb4c2698d06cc3b0dc80ce3f6756
José Benavente <jose@bgm.cl>
2023-10-16 03:31:03 -0300

    Fix imports

--- src/main/java/cl/bgm/Library/controller/AuthorsController.java
+++ src/main/java/cl/bgm/Library/controller/AuthorsController.java
@@ -2,11 +2,18 @@ package cl.bgm.Library.controller;
 
 import cl.bgm.Library.model.Author;
 import cl.bgm.Library.sevice.AuthorsService;
-import java.util.*;
+import java.util.Collection;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
--- src/main/java/cl/bgm/Library/model/Author.java
+++ src/main/java/cl/bgm/Library/model/Author.java
@@ -2,7 +2,14 @@ package cl.bgm.Library.model;
 
 import java.util.HashSet;
 import java.util.Set;
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
